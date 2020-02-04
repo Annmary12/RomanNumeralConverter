43eee79f632406802fe80e602ce574cf4f77b214
Annmary12 <annmaryamaka@yahoo.com>
2020-02-04 13:42:08 +0100

    add input number

---
+++ src/components/input.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import {InputGroup,
+  InputGroupButtonDropdown,
+  Input,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+interface IProps {
+  onChange: any, 
+  name: string, 
+  value: string | number, 
+  placeholder?: string,
+  buttonLabel: string,
+  handleSubmit: any
+}
+const InputComponent: React.FC<IProps> = ({ name, onChange, value, placeholder, buttonLabel, handleSubmit }) => {
+  const [dropdownOpen, setDropdownOpen] = React.useState(false);
+
+  const toggleDropDown = () => setDropdownOpen(!dropdownOpen);
+  return (
+    <InputGroup>
+      <Input name={name} value={value} onChange={onChange} placeholder={placeholder} required/>
+      <InputGroupButtonDropdown addonType="append" isOpen={dropdownOpen} toggle={toggleDropDown}>
+            <DropdownToggle color='success' caret>
+              {buttonLabel}
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem onClick={handleSubmit ('romans')}>Convert To Roman</DropdownItem>
+              <DropdownItem onClick={handleSubmit('integer')}>Convert To Number</DropdownItem>
+            </DropdownMenu>
+          </InputGroupButtonDropdown>
+    </InputGroup>
+  )
+}
+
+export default InputComponent
